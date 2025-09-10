4ec2112e8aeb5e6f6007e58aa7897ac705d6d1de
Vicente Souza <vicentedesouza762@gmail.com>
2025-09-10 03:21:39 +0000

    fix: estrutura do projeto Spring Boot e classe principal adicionada

---
+++ spring-security-jwt-demo/src/main/java/com/vicente/auth/SpringSecurityJwtDemoApplication.java
@@ -0,0 +1,11 @@
+package com.vicente.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtDemoApplication.class, args);
+    }
+}
